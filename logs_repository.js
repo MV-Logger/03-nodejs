02a309f3fc8eb3b07ec027515ffaabdf190c32b7
maarten.vercruysse <maarten.vercruysse@student.howest.be>
2021-05-22 12:29:19 +0200

    Convert repositories to Postgresql instead of mysql

--- logs_repository.js
+++ logs_repository.js
@@ -2,41 +2,40 @@ const query = require("./connection.js").query
 
 async function getBooks(user) {
     return await query(`select *
-                  from books
-                  where user_id = ?`, user);
+                        from books
+                        where user_id = $1`, user);
 }
 
 async function getBook(id) {
     return await query(`select *
                         from books
-                        where id = ?`, id)
+                        where id = $1`, id)
 }
 
 async function getEntries(book) {
     return await query(`select *
                         from entries
-                        where book_id = ?`, book);
+                        where book_id = $1`, book);
 }
 
 async function addEntry(bid, text, where, when) {
-    return await query("insert into entries set ?", {book_id: bid, text: text, where: where, when: when});
+    return await query("insert into entries(book_id, text, `where`, `when` ) values($1, $2, $3, $4)", bid, text, where, when);
 }
 
 async function addBook(name, creator) {
-    return await query(`insert into books
-                        set ?`, {name: name, user_id: creator});
+    return await query("insert into books(`name`, user_id ) VALUES($1, $2)", name, creator);
 }
 
 async function deleteBook(id) {
     return await query(`delete
                         from books
-                        where id = ?`, id)
+                        where id = $1`, id)
 }
 
 async function updateBook(id, newName) {
     return await query(`update books
-                        set name = ?
-                        where id = ?`, newName, id)
+                        set "name" = $1
+                        where id = $2`, newName, id)
 }
 
 module.exports = {
--- user_repository.js
+++ user_repository.js
@@ -1,19 +1,19 @@
 const query = require("./connection.js").query;
 
 async function registerUser(username, pass) {
-    return await query(`insert into users
-                        set ?`, {username: username, password: pass});
+    return await query(`insert into users(username, password)
+                        values($1, $2)`, username, pass);
 }
 
 async function login(username) {
     return await query(`select id, password
                                 from users
-                                where LOWER(username) = LOWER(?)
+                                where LOWER(username) = LOWER($1)
                                 `, username);
 }
 
 async function existUsername(username) {
-    return (await query("select * from users where LOWER(username) = LOWER(?)", username)).length === 1
+    return (await query("select * from users where LOWER(username) = LOWER($1)", username)).length === 1
 }
 
 module.exports = {
